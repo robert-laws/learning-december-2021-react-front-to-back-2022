086d6b2c4c233cfaff7e6b272c84615a80b64899
Robert Laws <roblaws@me.com>
2021-12-11 18:59:44 +0300

    Add cards and basic feedback logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData.js';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete this feedback?')) {
+      setFeedback(feedback.filter((feedback) => feedback.id !== id));
+    }
+  };
+
   return (
     <>
       <Header />
       <div className='container'>
+        <FeedbackStats feedback={feedback} />
         <h1>My App</h1>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
