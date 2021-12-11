171532a5634492ed0d4674a7d67df3353fb9d767
Robert Laws <roblaws@me.com>
2021-12-11 14:47:49 +0300

    add basic header with styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
+import Header from './components/Header';
+
 function App() {
-  const title = 'Blog Post';
-  const body = 'This is my new blog post';
-  const comments = [
-    { id: 1, text: 'hello there' },
-    { id: 2, text: 'welcome friend' },
-    { id: 3, text: 'howdy buddy' },
-  ];
-
   return (
-    <div className='container'>
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      {comments.length > 0 && (
-        <div className='comments'>
-          <h3>Comments ({comments.length})</h3>
-          <ul>
-            {comments.map((comment) => {
-              return <li key={comment.id}>{comment.text}</li>;
-            })}
-          </ul>
-        </div>
-      )}
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
