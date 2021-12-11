7faeaf73671c708e5d9ea3ee23c22b13994274f5
Robert Laws <roblaws@me.com>
2021-12-11 14:19:43 +0300

    basic jsx example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 function App() {
-  return <h1>Hello from the App Component</h1>;
+  const title = 'Blog Post';
+  const body = 'This is my new blog post';
+  const comments = [
+    { id: 1, text: 'hello there' },
+    { id: 2, text: 'welcome friend' },
+    { id: 3, text: 'howdy buddy' },
+  ];
+
+  return (
+    <div className='container'>
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      {comments.length > 0 && (
+        <div className='comments'>
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((comment) => {
+              return <li key={comment.id}>{comment.text}</li>;
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
